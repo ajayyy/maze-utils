7f53d7e6dfc817a128cd319d68839b4511595c6f
Michael C <michael@mchang.name>
2024-05-01 01:42:19 -0400

    add more specific selector

--- src/elements.ts
+++ src/elements.ts
@@ -1,6 +1,6 @@
 export function getYouTubeTitleNodeSelector(): string {
     // New YouTube Title, YouTube, Mobile YouTube, Invidious, YouTube videoPrimaryInfoRenderer (2021) layout
-    return "#title h1, .ytd-video-primary-info-renderer.title, .slim-video-information-title, #player-container + .h-box > h1, h1.title";
+    return "#title h1, .ytd-video-primary-info-renderer.title, .slim-video-information-title, #player-container + .h-box > h1, .ytd-video-primary-info-renderer > h1.title";
 }
 
 export function getYouTubeTitleNode(): HTMLElement {
