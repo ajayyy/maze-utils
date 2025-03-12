5cfc732c014a522691aeb6262fc87b333a51087d
Ajay <dev@ajay.app>
2025-03-12 03:02:38 -0400

    Don't run thumbnail listener code on youtube tv

--- src/pageInfo.ts
+++ src/pageInfo.ts
@@ -2,6 +2,10 @@ export function onMobile() {
     return window.location.hostname === "m.youtube.com";
 }
 
+export function onYouTubeCableTV() {
+    return window.location.hostname === "tv.youtube.com";
+}
+
 export function onVideoPage() {
     return !!document.URL.match(/\/watch|\/shorts|\/live|\/embed/);
 }
