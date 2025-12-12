76a38ef37dd1c992acc25beacbd9b1e874ef778c
Ajay <dev@ajay.app>
2025-12-12 03:42:57 -0500

    Remove unnecessary comment

--- src/index.ts
+++ src/index.ts
@@ -58,9 +58,6 @@ export function timeoutPomise<T>(timeout?: number): Promise<T> {
     });
 }
 
-/**
-* web-extensions
-*/
 const onFirefoxOrSafari = typeof(chrome) !== "undefined" && !!chrome.runtime.getManifest().browser_specific_settings;
 const onFirefox = typeof(chrome) !== "undefined" && !!chrome.runtime.getManifest().browser_specific_settings?.gecko;
 export function isFirefox(): boolean {
